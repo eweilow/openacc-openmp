d631ee7b75acd299cf87d8f7044f0272f6797593
E. Weilow <eweilow@digitalkoppar.se>
2018-11-27 02:12:36 -0600

    Can multiply big matrices

--- matrix/main.c
+++ matrix/main.c
@@ -1,84 +1,69 @@
 #include <time.h>
-#include <openacc.h>
+#include <math.h>
 
-#define LENGTH 100000
-#define BATCH_ROWS LENGTH
-#define BATCH_COLUMNS 5000
+#include "./shared.c"
 
-//int a[LENGTH][LENGTH];
-float b[LENGTH];
-float c[LENGTH];
+#define BLOCK_SIZE 200000
+float inputVector[MATRIX_SIZE];
+float outputVector[MATRIX_SIZE];
+
+void multiplyMatrixBlock(int fromRow, int toRow, int fromColumn, int toColumn)
+{
+#pragma acc parallel loop present(outputVector, inputVector)
+  for (int row = fromRow; row < toRow; ++row)
+  {
+    float sum = 0.0;
+#pragma acc loop vector reduction(+ \
+                                  : sum)
+    for (int column = fromColumn; column < toColumn; ++column)
+    {
+      sum += matrixElement(row, column) * inputVector[column];
+    }
+    outputVector[row] += sum;
+  }
+}
+
+void matrixMultiply()
+{
+#pragma acc data copy(outputVector) copyin(inputVector)
+  for (int i = 0; i <= MATRIX_SIZE / BLOCK_SIZE; i++)
+  {
+    int startI = i * BLOCK_SIZE;
+    int endI = (i + 1) * BLOCK_SIZE;
+    if (endI >= MATRIX_SIZE)
+    {
+      endI = MATRIX_SIZE;
+    }
+    for (int j = 0; j <= MATRIX_SIZE / BLOCK_SIZE; j++)
+    {
+      int startJ = j * BLOCK_SIZE;
+      int endJ = (j + 1) * BLOCK_SIZE;
+      if (endJ >= MATRIX_SIZE)
+      {
+        endJ = MATRIX_SIZE;
+      }
+      multiplyMatrixBlock(startI, endI, startJ, endJ);
+    }
+  }
+}
 
 int main()
 {
+  printf("Initializing vectors\n");
+  initializeVectors(inputVector, outputVector);
   printf("Starting matrix multiplication\n");
-
-  for (int i = 0; i < LENGTH; i++)
-  {
-    for (int j = 0; j < LENGTH; j++)
-    {
-      //a[i][j] = i == j ? 1 : 0;
-    }
-  }
-
-  for (int i = 0; i < LENGTH; i++)
-  {
-    b[i] = (float)i;
-    c[i] = 0.0;
-  }
-
   clock_t begin = clock();
 
-  // 00 + 11 + 22 + ... + nn
-  // (00 + 11 + 22 + ... + kk) + () + ... + () + (mm + .. + nn)
-  /*
-  c0 = a00 b0 + a10 b1
-  c1 = a01 b0 + a11 b1
-*/
-  for (int k = 0; k < LENGTH / BATCH_COLUMNS; k++)
-  {
-    int fromJ = k * BATCH_COLUMNS;
-    int toJ = k * BATCH_COLUMNS + BATCH_COLUMNS;
-    printf("%d / %d\n", k, LENGTH / BATCH_COLUMNS);
-    //printf("%d / %d\n %d ... %d (%d columns)\n", k, LENGTH / BATCH_COLUMNS, fromJ, toJ, toJ - fromJ);
-    for (int l = 0; l < LENGTH / BATCH_ROWS; l++)
-    {
-      int fromI = l * BATCH_ROWS;
-      int toI = l * BATCH_ROWS + BATCH_ROWS;
-
-//#pragma acc loop copyin(b [fromJ:toJ]) create(c [fromI:toI]) copyout(c [fromI:toI]) private(sum)
-#pragma acc kernels copyin(b) copyout(c)
-      {
-#pragma acc loop worker
-        for (int i = fromI; i < toI; ++i)
-        {
-          float sum = 0.0;
-#pragma acc loop vector reduction(+ \
-                                  : sum)
-          for (int j = fromJ; j < toJ; j++)
-          {
-            sum += (i == j ? 1.0 : 0.0) * b[j];
-          }
-
-#pragma acc atomic
-          c[i] += sum;
-        }
-      }
-    }
-  }
+  matrixMultiply();
 
   clock_t end = clock();
-  printf("Computation took %f ms\n", 1000.0 * (double)(end - begin) / CLOCKS_PER_SEC);
+  printf("\n\nComputation took %f ms\n", 1000.0 * (double)(end - begin) / CLOCKS_PER_SEC);
+  printf("Input: ");
+  printVector(inputVector);
+  printf("Output: ");
+  printVector(outputVector);
+  compareVectors(inputVector, outputVector);
+  printf("\n\n");
 
-  for (int i = 0; i < 10; i++)
-  {
-    printf("%d ", b[i]);
-  }
-  printf("\n");
-  for (int i = 0; i < 10; i++)
-  {
-    printf("%d ", c[i]);
-  }
-  printf("\n");
   return 0;
 }
---
+++ matrix/shared.c
@@ -0,0 +1,42 @@
+#define MATRIX_SIZE 500000
+
+void initializeVectors(float a[MATRIX_SIZE], float b[MATRIX_SIZE])
+{
+  for (int i = 0; i < MATRIX_SIZE; i++)
+  {
+    a[i] = (float)i;
+    b[i] = 0.0;
+  }
+}
+
+float matrixElement(int row, int column)
+{
+  if (row == column)
+  {
+    return 1.0;
+  }
+  return 0.0;
+}
+
+void printVector(float vec[MATRIX_SIZE])
+{
+  const int standardSize = 10;
+  int size = MATRIX_SIZE < standardSize ? MATRIX_SIZE : standardSize;
+  for (int i = 0; i < size; i++)
+  {
+    printf("%.2f ", vec[i]);
+  }
+  printf("\n");
+}
+
+void compareVectors(float a[MATRIX_SIZE], float b[MATRIX_SIZE])
+{
+  for (int i = 0; i < MATRIX_SIZE; i++)
+  {
+    if (fabs(a[i] - b[i]) > 0.001)
+    {
+      printf("mismatch at index %d: %f != %f\n", i, a[i], b[i]);
+    }
+  }
+  printf("\n");
+}
