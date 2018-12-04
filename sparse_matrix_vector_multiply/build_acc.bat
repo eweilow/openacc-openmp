c827b2900b8216f5559d9885ac49125fe2da54c6
E. Weilow <eweilow@digitalkoppar.se>
2018-12-04 10:55:58 +0100

    Set right acceleration

--- sparse_matrix_vector_multiply/build_acc.bat
+++ sparse_matrix_vector_multiply/build_acc.bat
@@ -3,7 +3,7 @@
 SET PGI_ACC_TIME=0
 SET OUTPUT=".\dist\build_acc.exe"
 SET PGCC="pgcc"
-SET BUILDCOMMAND=%PGCC% -o=%OUTPUT% -acc -ta=multicore -O3 -fast main.c
+SET BUILDCOMMAND=%PGCC% -o=%OUTPUT% -acc -ta=tesla -O3 -fast main.c
 
 echo Removing existing build
 del %OUTPUT%
